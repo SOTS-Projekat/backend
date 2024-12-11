614aea7f85685fd3c62b213e15f846886daa8d2e
DusanVukovic3 <vukoviduan047@gmail.com>
2024-12-11 11:31:13 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,4 @@ INSERT INTO app_user (id, username, password, email, role) values (3, 'student1'
 INSERT INTO app_user (id, username, password, email, role) values (4, 'student2', '$2a$10$EIXRkF6Hk9s.F3e5ugNSG.7u9T0cJdQaUL1qUzEl5boX8aAbeXiZe', 'student2@example.com', 'STUDENT'),, -- password is 'password'
 INSERT INTO app_user (id, username, password, email, role) values (5, 'student3', '$2a$10$EIXRkF6Hk9s.F3e5ugNSG.7u9T0cJdQaUL1qUzEl5boX8aAbeXiZe', 'student3@example.com', 'STUDENT'); -- password is 'password'
 
+--
